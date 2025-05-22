8b2acbb4b564d04f0e36ea988b64abecb34aaa68
Ciel <jungturn01tw@gmail.com>
2025-05-23 03:00:03 +0800

    feat: 新增 footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,19 @@
+import { memo } from 'react';
+import Logo from '@/components/Logo';
+
+const Footer = memo(() => {
+  return (
+    <footer className="bg-black">
+      <section>
+        <div className="container-sm flex flex-col justify-center items-center">
+          <Logo />
+          <p className="mt-5 pt-5 border-t border-solid border-background w-full text-center text-xs">Copyright © 2025 Movie Seek. All rights reserved.</p>
+        </div>
+      </section>
+    </footer>
+  );
+});
+
+Footer.displayName = 'Footer';
+
+export default Footer;
