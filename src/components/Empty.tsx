be256bba96c133b2c0aef475d0a46760a5866f8c
Ciel <jungturn01tw@gmail.com>
2025-05-23 00:13:17 +0800

    feat: 新增 Empty

---
+++ src/components/Empty.tsx
@@ -0,0 +1,19 @@
+import { memo } from 'react';
+import Image from 'next/image';
+
+const Empty = memo(() => {
+  return (
+    <div className="flex flex-col h-[200px] justify-center items-center text-muted-foreground space-y-3">
+      <Image
+        src="/icons/not-found.svg"
+        alt="not-found"
+        width={80}
+        height={80}
+      />
+      <p>暫無資料</p>
+    </div>
+  );
+});
+
+Empty.displayName = 'Empty';
+export default Empty;
