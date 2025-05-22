fca7b3444cf7ad58470989e43cfb6a039eaf52cc
Ciel <jungturn01tw@gmail.com>
2025-05-23 00:00:59 +0800

    refactor: 調整 header layout

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,10 +3,12 @@ import Logo from '@/components/Logo';
 
 const Header = memo(() => {
   return (
-    <header className="fixed w-full border-b border-b-foreground px-4 py-3 flex items-center top-0 z-20 bg-gradient-to-b from-background to-transparent">
-      <div className="container">
-        <Logo />
-      </div>
+    <header className="fixed w-full border-b border-b-foreground flex items-center top-0 z-20 bg-gradient-to-b from-background to-transparent">
+      <section className="!py-3 w-full">
+        <div className="container">
+          <Logo />
+        </div>
+      </section>
     </header>
   );
 });
