269eaec4b658bccd3ab4770542ff92d30236570a
Ciel <jungturn01tw@gmail.com>
2025-05-23 23:14:36 +0800

    feat: 新增 use post

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,6 +1,7 @@
 'use client';
 import useSWR from 'swr';
 import axios from '@/lib/axios';
+import { useState } from 'react';
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
@@ -10,4 +11,33 @@ export const useGet = <T>(endpoint: string | null) => {
   });
 
   return { data, error, isLoading };
+};
+
+type UsePostResult<TResponse> = {
+  data: TResponse | null;
+  error: Error | null;
+  isLoading: boolean;
+  post: (body?: any) => Promise<void>;
+};
+
+export const usePost = <TResponse = unknown>(endpoint: string): UsePostResult<TResponse> => {
+  const [data, setData] = useState<TResponse | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const post = async (body?: any) => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await axios.post<TResponse>(endpoint, body);
+      setData(response.data);
+    } catch (err: any) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { data, error, isLoading, post };
 };
